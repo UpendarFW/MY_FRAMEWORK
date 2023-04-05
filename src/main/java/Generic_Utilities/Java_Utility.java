95cbf8dabc76df62051f443f9327bb40a60f94bb
upend <upend@UPENDAR1998>
2023-04-05 22:39:11 +0530

    Java Utility Committed

--- src/main/java/Generic_Utilities/Java_Utility.java
+++ src/main/java/Generic_Utilities/Java_Utility.java
@@ -1,5 +1,12 @@
 package Generic_Utilities;
 
+import java.util.Random;
+
 public class Java_Utility {
 
+	public int gatRandomNumber() {
+		Random r=new Random();
+		int random=r.nextInt(10000);
+		return random;
+	}
 }
